e38e891ecc3a662bce795a04629d87996227b2b5
778604 <selvapandy.m@gmail.com>
2016-11-24 07:49:02 -0500

    s

--- vs-menu-model/src/main/java/com/vs/menu/models/Menu.java
+++ vs-menu-model/src/main/java/com/vs/menu/models/Menu.java
@@ -11,7 +11,8 @@ import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 //Pandy adding this change
- //Ok- Selva accepted the change
+ //Ok- Selva accepted the change 
+
 //Ok hi
 @Entity
 @Table(name="VS_MENU")
